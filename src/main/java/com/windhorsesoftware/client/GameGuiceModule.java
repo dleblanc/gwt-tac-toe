955a0125ac759bcb8a1e19a686a9103295c73eca
Dave LeBlanc <david.leblanc@gmail.com>
2011-12-28 15:08:17 -0800

    Updated the classpath to use JDK 1.6, killed a couple unused launch files, removed some unused code from the module

--- src/main/java/com/windhorsesoftware/client/GameGuiceModule.java
+++ src/main/java/com/windhorsesoftware/client/GameGuiceModule.java
@@ -1,14 +1,11 @@
 package com.windhorsesoftware.client;
 
 import com.google.gwt.inject.client.AbstractGinModule;
-import com.google.inject.Singleton;
 import com.windhorsesoftware.tictactoe.GameView;
 
 class GameGuiceModule extends AbstractGinModule {
 	@Override
 	protected void configure() {
-		// For brevity sake, just wire everything up as singleton
-		bind(GameView.class).to(GwtGameView.class); //.in(Singleton.class);
-		//bind(GamePresenter.class).in(Singleton.class);
+		bind(GameView.class).to(GwtGameView.class);
 	}
 }
